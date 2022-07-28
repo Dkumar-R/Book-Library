0f044fb2d22c5a11f0cb5de461753e1c14b29ebc
Dhananjay Kumar <anythinkworld@gmail.com>
2022-07-28 16:28:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Our Book Library was");
+
+function Book(title, auther, type){
+    this.title = title;
+    this.auther = auther;
+    this.type = type;
+
+}
+
+// Display 
+function Display(){
+
+}
+
+// Add method to display prototype.
+
+
+
+
+// Add submit event when you clicked
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit',libraryFormSubmit);
+function libraryFormSubmit(e){
+    e.preventDefault();
+    console.log('You have click submit');
+}
